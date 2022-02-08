92221c73e0cdda7b47b8aa634938e61cd0064aa6
Fredrick Burgess <Fredrick.l.Burgess@gmail.com>
2022-02-08 16:11:31 -0500

    commit tree a41039829f6aed8fed842357236cdff1335cef0d

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let num3 = 10
+let num4 = 6
+let multiply = (num1 * num2); 
 
-// Write your code here
+let random = 4;
+
+let mod = (num3 % num4); 
+
+let max = (2, 5, 18, 20)
+ Math.max(max); 
